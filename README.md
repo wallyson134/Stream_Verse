6767de53bbf6920c1a95cd7e76d0d99a9dd78002
wallyson134 <fwnsilva@aluno.uespi.br>
2023-11-18 23:32:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TwitchStream
+Twitch streaming website
+
+Create a website for your twitch stream with ease.
+
+Change `twitchusername` in `index.html` and `js/main.js` and it will automatically include your twitch feed.
+
+Demo: http://friss.github.io/TwitchStream/
+
+Previews
+![Preview](https://raw.github.com/friss/twitchstream/master/preview.png)
+![Preview](https://raw.github.com/friss/twitchstream/master/preview2.png)
---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Change this to be your own clientId from https://www.twitch.tv/kraken/oauth2/clients/new
+var clientId = "a7aag1oiocd1vxl0h9jvd2sahmaq5j0";
+
+$.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ clientId + "&callback=?", function(response) {
+  if (response.stream){
+    $('.viewers span').text(response.stream.viewers);
+    $('.intro iframe').removeClass("hidden");
+    $('.intro-text').addClass('hidden');
+  }else{
+    $('.viewers span').text("0");
+  }
+});
+
+$.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?client_id="+ clientId + "&callback=?", function(response) {
+  $('.followers span').text(response["_total"]);
+});
